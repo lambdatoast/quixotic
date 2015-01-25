fc5481d0f04ff1f9f277df07e423b245bf22dd02
Alexander Noriega <lambdatoast@gmail.com>
2015-01-25 14:06:14 -0300

    Improve FunctorTest.

--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -6,19 +6,22 @@ class MaybeTest extends \PHPUnit_Framework_TestCase
     {
       $x = new Some(42);
       $f = function ($n) {
-        return $n * 2;
+        return $n + 1;
       };
       $g = function ($n) {
-        return $n / 2;
+        return $n + 2;
       };
       $composed_maps = Basics::compose(
         function ($x) use ($f) { return $x->map($f); },
         function ($x) use ($g) { return $x->map($g); }
       );
-      $this->assertTrue($x->map(array('Basics', 'id'))->equal(Basics::id($x)));
       $this->assertTrue(
-        $x->map(Basics::compose($f, $g))
-          ->equal($composed_maps($x))
+        $x->map(array('Basics', 'id'))->equal(Basics::id($x)),
+        'First functor law: Mapping id is a no-op.'
+      );
+      $this->assertTrue(
+        $x->map(Basics::compose($f, $g))->equal($composed_maps($x)),
+        'Second functor law: Mapping a composed function is the same as mapping each function.'
       );
     }
 }
