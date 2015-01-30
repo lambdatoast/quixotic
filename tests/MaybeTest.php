245abbbdd51840b7c0b61c6b3be5f824bc30f44f
Alexander Noriega <lambdatoast@gmail.com>
2015-01-29 21:50:51 -0300

    Add tests for Maybe's Equal and Alternative implementations.

--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -24,5 +24,48 @@ class MaybeTest extends \PHPUnit_Framework_TestCase
         'Second functor law: Mapping a composed function is the same as mapping each function.'
       );
     }
-}
 
+    public function testEqual() {
+      $this->assertTrue(
+        (new None)->equal(new None),
+        'Maybe Equality: Nones are equal.'
+      );
+
+      $this->assertFalse(
+        (new None)->equal(new Some(42)),
+        'Maybe Equality: Nones and Somes cannot be equal.'
+      );
+
+      $this->assertTrue(
+        (new Some(42))->equal(new Some(42)),
+        'Maybe Equality: Somes of same value are equal.'
+      );
+
+      $this->assertFalse(
+        (new Some(41))->equal(new Some(42)),
+        'Maybe Equality: Somes of different value cannot be equal.'
+      );
+
+    }
+
+    public function testAlternative() {
+      $this->assertEquals(
+        new Some(42),
+        (new None)->or_(new Some(42)),
+        'Maybe Alternative: None OR Some(v) = Some(v)'
+      );
+
+      $this->assertEquals(
+        new Some(42),
+        (new Some(42))->or_(new None),
+        'Maybe Alternative: Some(v) OR None = Some(v)'
+      );
+
+      $this->assertEquals(
+        new None,
+        (new None)->or_(new None),
+        'Maybe Alternative: None OR None = None'
+      );
+    }
+
+}
